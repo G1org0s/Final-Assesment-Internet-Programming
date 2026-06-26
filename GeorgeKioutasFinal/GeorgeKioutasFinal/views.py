5a1c54ccd1ea06198d795f4e659634a5b22dcf2a
GKioutas <g.kioutas@acg.edu>
2026-06-26 16:27:35 +0300

    First Commit of Final Assesment still needs a lot of work to be done (Initializing Repository)

---
+++ GeorgeKioutasFinal/GeorgeKioutasFinal/views.py
@@ -0,0 +1,15 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+  #  return HttpResponse("Hello World! This is the Home Page.")
+  return render(request, 'home.html')
+
+
+def about(request):
+  #  return HttpResponse("This is the about page.")
+  return render(request, 'about.html')
+
+
+def index(request):
+    return render(request, "tasks.html")
